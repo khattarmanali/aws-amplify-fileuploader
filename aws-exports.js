49b0e91c3d12579a6b46f09561ebd04bdd10048f
khattarmanali <manalikhattar1@gmail.com>
2023-02-20 17:21:29 +0530

    file uploder implemented

---
+++ aws-exports.js
@@ -0,0 +1,14 @@
+const config = {
+  Auth: {
+    identityPoolId: "ap-south-1:304d8b80-a0de-479c-bab6-eb389b5718e8",
+    region: "ap-south-1",
+    userPoolId: "ap-south-1_etyOj6wn3",
+    userPoolWebClientId: "49km5c410cpvog0el9j3evg6vs",
+  },
+  Storage: {
+    bucket: "demo-eventfix-filemanager-bucket",
+    region: "ap-south-1",
+    identityPoolId: "ap-south-1:304d8b80-a0de-479c-bab6-eb389b5718e8",
+  },
+};
+export default config;
